58a499087b4cab419ef8add1aac7febfbb370f75
Felipa Mendez <felipalouise@gmail.com>
2022-01-18 11:24:33 -0600

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+let form = document.querySelector('form').addEventListener('submit', (e) => {e.preventDefault()
+  buildTask(e.target.new_task_description.value) 
+  form.reset()
+  })
+      
+
 });
+
+
+
+function buildTask(task) { 
+  let p = document.createElement('p')
+  let btn = document.createElement('button')
+  btn.addEventListener('click', handleDelete)
+  btn.textContent = "x"
+  p.textContent = `${task} ` 
+  p.appendChild(btn)
+  document.querySelector('#tasks').appendChild(p)
+  }
+
+  function handleDelete(e) {
+    e.target.parentNode.remove()
+    }
+    
+
